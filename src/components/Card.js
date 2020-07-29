6657b542f3d9b155f4c3768a06fb2759151902e8
Mollie-S <olcha.olcha@gmail.com>
2020-07-29 22:31:23 +0300

    The back side of a card is being shown at the app load.

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="fruit-card" name={props.name}>
-      {!props.cardOpen ? (
+    <div className="fruit-card" name={props.name} onClick={props.handleClick}>
+      {props.cardOpen ? (
         <img className="card-front" src={props.src} alt={`${props.name}`}></img>
       ) : (
-        <div className="card-back">1</div>
+        <div className="card-back"></div>
       )}
     </div>
   );
