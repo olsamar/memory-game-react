46a62ae06108984f329a2b834460462f780c3a3b
Mollie-S <olcha.olcha@gmail.com>
2020-08-02 12:32:02 +0300

    Implemented check on the first and second cards flipped. No more than 2 cards are flipped at the deck.

--- src/components/DisplayContainer.js
+++ src/components/DisplayContainer.js
@@ -17,18 +17,50 @@ const fruits = [
   { name: "Strawberries", source: Strawberries },
 ];
 function DisplayContainer() {
-  const initialState = fruits
+  const gameFieldInitialState = fruits
     .concat(fruits)
     .sort(() => Math.random() - 0.5)
-    .map((fruit) => ({ open: false, card: fruit }));
-  const [gameField, setGameField] = useState(initialState);
-  console.log(gameField);
+    .map((fruit) => ({ open: false, card: fruit, matched: false }));
+  const [gameField, setGameField] = useState(gameFieldInitialState);
 
   const handleClick = (index) => {
     const newGameField = [...gameField];
-    newGameField[index].open = !newGameField[index].open;
     setGameField(newGameField);
+    flipTheCard(index, newGameField);
   };
+
+  const flipTheCard = (index, newGameField) => {
+    const cardClicked = newGameField[index];
+    if (
+      !cardClicked.matched &&
+      newGameField.reduce((amountFlipped, cell) => {
+        return cell.open === true ? amountFlipped + 1 : amountFlipped;
+      }, 0) === 0
+    ) {
+      cardClicked.open = true;
+    } else if (
+      !cardClicked.matched &&
+      newGameField.reduce((amountFlipped, cell) => {
+        return cell.open === true ? amountFlipped + 1 : amountFlipped;
+      }, 0) === 1
+    ) {
+      const firstCardFlipped = newGameField.find((card) => card.open === true);
+      cardClicked.open = true;
+      matchThePair(cardClicked, firstCardFlipped);
+      // console.log(firstCardFlipped);
+    } else {
+      return;
+    }
+  };
+
+  const matchThePair = (cardClicked, firstCardFlipped) => {
+    if ((cardClicked.card.name = firstCardFlipped.card.name)) {
+      cardClicked.matched = true;
+      firstCardFlipped.matched = true;
+    }
+    console.log(cardClicked, firstCardFlipped);
+  };
+
   return (
     <div className="container">
       {gameField.map((cell, index) => (
