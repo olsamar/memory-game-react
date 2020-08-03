119ccbaee3adfb4d91d1987e0088309ec4c48172
Mollie-S <olcha.olcha@gmail.com>
2020-08-03 15:09:43 +0300

    Matching cards changing opacity.

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,9 @@ function Card(props) {
       style={
         props.cardOpen === CardState.MATCHED
           ? {
-              animation: "matchingCardsScaled 1s ease-out 0.5s",
+              animation: "matchingCardsScaled 1s ease-in-out 0.5s",
+              transition: "opacity 1.5s linear 0.5s",
+              opacity: "0.7",
             }
           : {}
       }
