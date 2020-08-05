e412dee88add8efa73d31f958d3d0d00dffa038f
Mollie-S <olcha.olcha@gmail.com>
2020-08-05 18:55:31 +0300

    Added more styles and media queries for different viewport sizes.

--- src/components/EndOfTheGame.js
+++ src/components/EndOfTheGame.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./EndOfTheGame.css";
 
 export default function EndOfTheGame(props) {
   return (
     <React.Fragment>
       {props.gameEnd ? (
         <div className="end-of-game">
+          <h1>YOU DID IT!</h1>
           <button className="play-again-btn" onClick={props.resetGame}>
             Play again
           </button>
