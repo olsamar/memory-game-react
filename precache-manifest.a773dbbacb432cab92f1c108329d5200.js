83644269ebb44a759140b2bec2776fbbbab7d2b8
Mollie-S <olcha.olcha@gmail.com>
2020-08-10 15:03:04 +0300

    Updates

---
+++ precache-manifest.a773dbbacb432cab92f1c108329d5200.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a444f8d2ab97b596bcf91798a61b3a2",
+    "url": "/memory-game-react/index.html"
+  },
+  {
+    "revision": "419bb0edf3a4bfff7efc",
+    "url": "/memory-game-react/static/css/main.979b28c4.chunk.css"
+  },
+  {
+    "revision": "92b173649f64aec9f4b9",
+    "url": "/memory-game-react/static/js/2.ed970724.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-game-react/static/js/2.ed970724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419bb0edf3a4bfff7efc",
+    "url": "/memory-game-react/static/js/main.494badd4.chunk.js"
+  },
+  {
+    "revision": "4f0f1ddd5c3840fd19e3",
+    "url": "/memory-game-react/static/js/runtime-main.8c9f7104.js"
+  },
+  {
+    "revision": "e17283a8e3bc7e25ee07f0b29fea5bd8",
+    "url": "/memory-game-react/static/media/Apple.e17283a8.png"
+  },
+  {
+    "revision": "b22058607c106425fb6adb2f4cf0d83a",
+    "url": "/memory-game-react/static/media/Banana.b2205860.png"
+  },
+  {
+    "revision": "27936f86ce3cf4b65a38181fc892b02f",
+    "url": "/memory-game-react/static/media/Blueberries.27936f86.png"
+  },
+  {
+    "revision": "ad1c2684a5e535eeac22af6c0b1a5f55",
+    "url": "/memory-game-react/static/media/Grape.ad1c2684.png"
+  },
+  {
+    "revision": "42e76214fc3958044042432233130d18",
+    "url": "/memory-game-react/static/media/Pear.42e76214.png"
+  },
+  {
+    "revision": "9e53a9acc2d833ebd9775fafeb2a647b",
+    "url": "/memory-game-react/static/media/PurpleDottedBg.9e53a9ac.png"
+  },
+  {
+    "revision": "fa8416d4f86f59a6a69e49d01fbf1f8e",
+    "url": "/memory-game-react/static/media/Strawberrie.fa8416d4.png"
+  }
+]);
